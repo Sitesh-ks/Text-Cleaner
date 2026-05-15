fe4532182db119ec814b4122f228a27cfe806e3d
Sitesh Kumar Samal <112224704+Sitesh-ks@users.noreply.github.com>
2026-05-15 12:26:13 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -269,7 +269,7 @@ export default function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Coded by @berkindev
+          Coded by @sks
         </a>
       </footer>
     </div>
